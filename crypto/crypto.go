3e657710a2b458e68c731b9b5a4ac462f6f79d3d
hani <dev.ahn.hani@gmail.com>
2026-04-07 19:43:45 -0300

    feat: add Verify Method

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,9 +1,14 @@
 package crypto
 
-import "crypto/subtle"
+import (
+	"crypto/subtle"
+
+	"github.com/seosoojin/dim/crypto/salt"
+)
 
 type Crypto interface {
-	HashString(src string) ([]byte, []byte, error)
+	HashString(src string) ([]byte, salt.Salt, error)
+	VerifyHash(password string, salt salt.Salt, expectedHash []byte) bool
 }
 
 func Compare(a, b []byte) bool {
